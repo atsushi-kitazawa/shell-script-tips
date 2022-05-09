0e7fbb9f1a7550933e9ca82ab5079ac93b4d9d9d
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-05-09 19:43:29 +0900

    add specific_size_log.sh

---
+++ specific_size_log.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#####
+# 指定されたサイズのログファイルを出力する
+#####
+
+MAX_FILE_SIZE=1024
+FILE_NAME="test.log"
+
+log=""
+for i in $(seq 1 100); 
+do 
+	log=$log"A" 
+done
+
+while true
+do
+	echo `date '+%Y/%m/%d %H:%M:%S'`" "$log >> $FILE_NAME
+	file_size_kb=`du -k $FILE_NAME | cut -f1`
+	if [ $file_size_kb -gt $MAX_FILE_SIZE ]; then
+		break
+	fi
+done
